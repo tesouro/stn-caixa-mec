9851c9462fb902c33015d5367fe6debec8c514f9
tiagombp <tiagombp@gmail.com>
2020-03-13 16:30:21 -0300

    adapta código para ler modelo de arquivo local, até configurar google cloud.

--- R/07-indicador_suspeita_de_empocamento.R
+++ R/07-indicador_suspeita_de_empocamento.R
@@ -2,9 +2,11 @@ library(tidymodels)
 library(tidyverse)
 
 # system("gcloud auth login")
-pins::board_register_gcloud(bucket = "ministerio_da_justica")
+#pins::board_register_gcloud(bucket = "ministerio_da_justica")
 
-modelo <- pins::pin_get("modelo_xgboost", board = "gcloud")
+#modelo <- pins::pin_get("modelo_xgboost", board = "gcloud")
+modelo <- readRDS("data/modelo.rds")
+
 ts_das_disponibilidades_liquidas_com_indicadores <- read_rds("data/ts_das_disponibilidades_liquidas_com_indicadores.rds")
 
 # base com scores
@@ -16,7 +18,7 @@ ts_das_disponibilidades_liquidas_com_indicadores_final <- ts_das_disponibilidade
   select(id, NO_UG, NO_ORGAO, NO_FONTE_RECURSO, serie_temporal, serie_temporal_random_crop, indicadores) %>%
   unnest(indicadores) %>%
   mutate(
-    suspeita_de_empocamento = predict(modelo, ., type = "prob")$.pred_Empoçamento,
+    suspeita_de_empocamento = predict(modelo, ., type = "prob")$.pred_Empoçamento
   )
 
 write_rds(ts_das_disponibilidades_liquidas_com_indicadores_final, "data/ts_das_disponibilidades_liquidas_com_indicadores_final.rds")
